c75f56d2ec15adf1feccdca567bcaa392595aff0
kalpesh doble <kalpeshdoble31@gmail.com>
2025-02-28 15:40:35 +0530

    [kalpesh] Add UC3_EmployeePayroll

--- UC3_EmployeePayroll/src/main/java/com/example/UC3_EmployeePayroll/Uc3EmployeePayrollApplication.java
+++ UC3_EmployeePayroll/src/main/java/com/example/UC3_EmployeePayroll/Uc3EmployeePayrollApplication.java
@@ -1,13 +1,13 @@
-package com.example.UC1_EmployeePayroll;
+package com.example.UC3_EmployeePayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Uc1EmployeePayrollApplication {
+public class Uc3EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Uc1EmployeePayrollApplication.class, args);
+		SpringApplication.run(Uc3EmployeePayrollApplication.class, args);
 	}
 
 }
--- UC3_EmployeePayroll/src/test/java/com/example/UC3_EmployeePayroll/Uc3EmployeePayrollApplicationTests.java
+++ UC3_EmployeePayroll/src/test/java/com/example/UC3_EmployeePayroll/Uc3EmployeePayrollApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.UC1_EmployeePayroll;
+package com.example.UC3_EmployeePayroll;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Uc1EmployeePayrollApplicationTests {
+class Uc3EmployeePayrollApplicationTests {
 
 	@Test
 	void contextLoads() {
